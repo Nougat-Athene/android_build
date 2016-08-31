6b42ea2a6eaf81e1dcbbf0fb1e45a29f7cfcabfa
Neil Agarwal <regalstreak@gmail.com>
2016-08-31 17:24:31 +0200

    roomservice: Change rev, branch, stuff

--- tools/roomservice.py
+++ tools/roomservice.py
@@ -36,14 +36,14 @@ except ImportError:
 # set this to the default remote to use in repo
 default_rem = "github"
 # set this to the default revision to use (branch/tag name)
-default_rev = "android-4.3"
+default_rev = "nougat-7.0"
 # set this to the remote that you use for projects from your team repos
-# example fetch="https://github.com/dirtyunicorns"
-default_team_rem = "du"
+# example fetch="https://github.com/Nougat-Athene"
+default_team_rem = "github"
 # this shouldn't change unless google makes changes
 local_manifest_dir = ".repo/local_manifests"
 # change this to your name on github (or equivalent hosting)
-android_team = "du"
+android_team = "github"
 
 
 def check_repo_exists(git_data):
@@ -202,7 +202,7 @@ def parse_device_from_folder(device):
 
 
 def parse_dependency_file(location):
-    dep_file = "du.dependencies"
+    dep_file = "aosp.dependencies"
     dep_location = '/'.join([location, dep_file])
     if not os.path.isfile(dep_location):
         print("WARNING: %s file not found" % dep_location)
